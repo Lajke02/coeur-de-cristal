35cce5925b2837ad4db6c3fae0e5a52ea7cb57c6
osmo23jf <148855720+osmo23jf@users.noreply.github.com>
2023-11-28 20:07:02 +0100

    update

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,11 @@
+// footer java script:
+// the following 6 lines of code was adapted from ChatGPT accessed 2023-11-28:
+const rows = document.querySelectorAll(".finfo .row");
 
+rows.forEach((row) => {
+  row.addEventListener("click", () => {
+    row.classList.toggle("active");
+  });
+});
+
+// specified styling, when you click on a row, the corresponding panel will expand or collapse. The active row will have the "active" class applied to it, which will make its panel visible:
