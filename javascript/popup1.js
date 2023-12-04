3bf56dda9c80f85f8a3475eb1d7f4c35e0722590
osmo23jf <148855720+osmo23jf@users.noreply.github.com>
2023-12-04 11:13:37 +0100

    update

--- javascript/popup1.js
+++ javascript/popup1.js
@@ -1,14 +1,13 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const purchaseButton = document.querySelector(".purchasebtn");
-  const modal = document.querySelector(".modal");
+function showModal() {
+  document.querySelector(".modal").style.display = "block";
+}
 
-  purchaseButton.addEventListener("click", function () {
-    modal.style.display = "block";
-  });
+function hideModal(event) {
+  if (event.target === document.querySelector(".modal")) {
+    document.querySelector(".modal").style.display = "none";
+  }
+}
 
-  modal.addEventListener("click", function (event) {
-    if (event.target === modal) {
-      modal.style.display = "none";
-    }
-  });
-});
+//onclick attribute on the purchase button calls the showModal function. The function selects the pop up element using document.querySelector and sets it to display style to block.
+
+//on click attribute on the popup calls the hidemModal function when clicked. hides the modalby setting display to none. The event object is passed to the hideModal function  to provide info about the click event
