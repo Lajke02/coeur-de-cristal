9f574e7a6ed281357594eecb5c5ae9aba897231a
alma23nz <149969050+alma23nz@users.noreply.github.com>
2023-12-05 16:15:37 +0100

    Update
    Updating the popup and the checkoutpage

--- javascript/detailcheckout.js
+++ javascript/detailcheckout.js
@@ -1,26 +1,8 @@
-const openPopupButton = document.getElementById("addtobagbutton");
-const closePopupButton = document.getElementById("checkoutbutton");
-const overlay = document.getElementById("overlay");
-const popupmodal = document.getElementById("popupmodal");
-
-openPopupButton.addEventListener("click", () => {
-  openPopup();
-});
-
-closePopupButton.addEventListener("click", () => {
-  closePopup();
-});
-
-overlay.addEventListener("click", () => {
-  closePopup();
-});
-
-function openPopup() {
-  popupmodal.classList.add("active");
-  overlay.classList.add("active");
+function showPopup() {
+  document.querySelector(".popupmodal").style.display = "block";
 }
-
-function closePopup() {
-  popupmodal.classList.remove("active");
-  overlay.classList.remove("active");
+function hidePopup(event) {
+  if (event.target === document.querySelector(".popupmodal")) {
+    document.querySelector(".popupmodal").style.display = "none";
+  }
 }
