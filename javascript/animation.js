a16793528ac134ae41c99a3ee033f78743bebf08
Lykke Karlsson <kaly23qb@student.ju.se>
2023-12-07 01:42:53 +0100

    animation and responsiveness

--- javascript/animation.js
+++
@@ -1,6 +0,0 @@
-const openPopupButton = document.getElementById("addtobagbutton");
-const shoppingbag = document.getElementById("shoppers");
-
-openPopupButton.addEventListener("click", () => {
-  shoppingbag.classList.toggle("ani");
-});
